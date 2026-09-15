f588068b9dd856664ac893c1b232ec394b71870c
kvaaishali <kondavaishali@klh.edu.in>
2026-09-15 20:37:02 +0530

    Add TextHackCorpus.java with cyber security information

---
+++ Week-1/articles/TextHackCorupus.java
@@ -0,0 +1,7 @@
+Cyber Security
+Michael Brown
+2026-03-10
+Cyber security protects computers, networks, and data from unauthorized access.
+Organizations use firewalls, encryption, and antivirus software to improve security.
+Cyber attacks such as phishing, malware, and ransomware continue to increase.
+Regular software updates and strong passwords help reduce security risks.
